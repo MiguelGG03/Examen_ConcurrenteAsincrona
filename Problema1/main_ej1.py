818be341dfa751a313bc7c3c068cae759e934349
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-25 00:57:11 +0200

    main hecho, falta una cosa en el clientes

--- Problema1/main_ej1.py
+++ Problema1/main_ej1.py
@@ -3,6 +3,35 @@ from Cliente import Cuenta
 from Movimiento import Movimiento
 import concurrent.futures
 
+def multiproceso_agregar(bank, NUM_PROCES,cantid):
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        #Agregación de todo el dinero
+        futures = [pool.submit(bank.agregar_dinero, "123",cantid) for _ in range(NUM_PROCES)]    
+
+        #Espera de que lleguen todos los procesos
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+                cuenta, cantidad = result
+                bank.agregar_dinero(cuenta, cantidad)
+            except Exception as e:
+                print(f"Error al ejecutar el proceso: {e}")
+
+
+def multiproceso_retirar(bank, NUM_PROCES,cantid):
+    with concurrent.futures.ProcessPoolExecutor() as pool:
+        #Agregación de todo el dinero
+        futures = [pool.submit(bank.retirar_dinero, "123",cantid) for _ in range(NUM_PROCES)]    
+
+        #Espera de que lleguen todos los procesos
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                result = future.result()
+                cuenta, cantidad = result
+                bank.agregar_dinero(cuenta, cantidad)
+            except Exception as e:
+                print(f"Error al ejecutar el proceso: {e}")
+
 
 def main_ej1():
 
@@ -14,18 +43,15 @@ def main_ej1():
     NUM_PROCES_2 = 20
     NUM_PROCES_3 = 60
 
-    with concurrent.futures.ProcessPoolExecutor() as pool:
-        #Agregación de todo el dinero
-        futures = [pool.submit(bank.agregar_dinero, "123",100) for _ in range(NUM_PROCES_1)]    
+    multiproceso_agregar(bank, NUM_PROCES_1, 100)
+    multiproceso_agregar(bank, NUM_PROCES_2, 50)
+    multiproceso_agregar(bank, NUM_PROCES_3, 20)
 
-        #Espera de que lleguen todos los procesos
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                result = future.result()
-                cuenta, cantidad = result
-                bank.agregar_dinero(cuenta, cantidad)
-            except Exception as e:
-                print(f"Error al ejecutar el proceso: {e}")
+    multiproceso_agregar(bank, NUM_PROCES_1, -100)
+    multiproceso_agregar(bank, NUM_PROCES_2, -50)
+    multiproceso_agregar(bank, NUM_PROCES_3, -20)
+
+    
 
     bank.ImprimeTodosLosMovimientos()
 
