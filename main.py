0f2ba118e8d995a0a39894b5cbe54fd621fd6ca8
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-25 01:07:51 +0200

    Ahora si que va ;D

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ def main():
     while resp=="S":
         resp2 = input("Elija el ejercicio a ejecutar (1,2): ")
         if resp2=="1":
+            print("\nEjecutando ejercicio 1...\n\n")
             main_ej1()
         elif resp2=="2":
             print("No esta hecho el ejercicio")
-        resp=input("Desea continuar? (S/N): ")
+        resp=input("\nDesea continuar? (S/N): ")
 
 if __name__ == "__main__":
     main()
