5203d4515fd97b40e7011204b2f97cb88a5c6be4
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-24 16:54:41 +0200

    Texto

--- clases/Cliente.py
+++ clases/Cliente.py
@@ -1,2 +1,11 @@
 class Cuenta:
-    pass
+    
+    def __init__(self, dni, saldo):
+        self.dni = dni
+        self.saldo = saldo
+
+    def getDni(self):
+        return self.dni
+    
+    def getSaldo(self):
+        return self.saldo
