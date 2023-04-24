f09dce3861bcf2d9c5838910bf1eccbaac7db1b6
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-24 16:53:49 +0200

    Banco tiene alguna cosas nuevas

--- clases/Banco.py
+++ clases/Banco.py
@@ -1,2 +1,29 @@
 class Banco:
-    pass
+    
+    def __init__(self):
+        self.__clientes = []
+        self.__mov = []
+
+    def getClientes(self):
+        return self.__clientes
+    
+    def getMovimientos(self):
+        return self.__mov
+
+    def agregar_cliente(self, cliente):
+        self.__clientes.append(cliente)
+    
+    def agregar_movimiento(self, movimiento):
+        self.__mov.append(movimiento)
+
+    def buscar_cliente(self, dni):
+        for cliente in self.__clientes:
+            if cliente.getDni() == dni:
+                return cliente
+        return None
+    
+    def buscar_movimiento(self, dni):
+        for movimiento in self.__mov:
+            if movimiento.getDni() == dni:
+                return movimiento
+        return None
