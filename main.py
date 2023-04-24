b06be7d8cb5c745ba8a86669c35d7b198fc04d20
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-24 18:22:57 +0200

    cambios en el main, sigue sin ir el banco

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from Problema1.main_ej1 import main_ej1
+
 def main():
     resp="S"
     while resp=="S":
-        print("Hola")
+        resp2 = input("Elija el ejercicio a ejecutar (1,2): ")
+        if resp2=="1":
+            main_ej1()
+        elif resp2=="2":
+            print("Ejercicio 2")
         resp=input("Desea continuar? (S/N): ")
 
 if __name__ == "__main__":
