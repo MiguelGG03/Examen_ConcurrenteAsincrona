7c21f842c0cf87762c94fc532e62181213553f03
migueliiin <91721552+migueliiin@users.noreply.github.com>
2023-04-25 10:44:59 +0200

    ADASDA

--- Problema2/Cliente.py
+++ Problema2/Cliente.py
@@ -1,1 +1,16 @@
+import random
+
 class Cliente:
+
+    def __init__(self):
+        self.__din= 1000
+        self.__num= random.randrange(0,36)
+
+    def getNum(self):
+        return self.__num
+    
+    def getDin(self):
+        return self.__din
+    
+    def pagoApuesta(self, dinero):
+        self.__din+= dinero
